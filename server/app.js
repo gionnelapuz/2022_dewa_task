7e0ba2af800c8382b4f5b22195ebbdb18d91b727
Gionne Lapuz <gionne.lapuz@gmail.com>
2022-01-23 04:09:26 +0400

    Upgraded size limit server side

--- server/app.js
+++ server/app.js
@@ -13,7 +13,7 @@ app.use(helmet());
 app.use(cookieParser());
 
 app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
+app.use(express.json({limit: '50mb'}));
 
 app.use(
   cors({
